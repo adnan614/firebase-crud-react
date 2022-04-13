b4aafc26c0102f9a4ad4e9a4ed3d3fb062a15700
adnan614 <adnanfaruque23@gmail.com>
2022-04-13 09:53:07 +0600

    update, delete in firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "./firebase-config";
-import { collection, getDocs,addDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 function App() {
   const [newName, setNewName] = useState("");
@@ -10,7 +17,18 @@ function App() {
   const usersCollectionRef = collection(db, "users");
 
   const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: newAge });
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
   };
 
   useEffect(() => {
@@ -44,6 +62,20 @@ function App() {
           <div>
             <h1>Name: {user.name}</h1>
             <h1>Age: {user.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              Delete user
+            </button>
           </div>
         );
       })}
